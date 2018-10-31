f94ba166ae45388592d1373b4f9990420f48cbcb
Matthew Wagar <mrw77@drexel.edu>
2018-10-31 13:18:22 -0400

    Editted the team page

--- app/controllers/app/team.js
+++ app/controllers/app/team.js
@@ -2,22 +2,73 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-    showDialog:false,
+    showDialog: false,
 
-    selectedProfileTab:"aboutme",
+    selectedProfileTab: "aboutme",
 
     profiles: Ember.computed(function () {
-        return [            
+        return [
             Ember.Object.create({
-            id: "mwagar",
-            pic: "assets/images/mwagar_profile_pic.jpg",
-            name: 'Matthew Wagar',
-            role: 'Unity Developer',
-            color: 'mwagar-color',
-            icon: 'extension',
-            myrole:"woah boy",
-            aboutme:"Test123"
-        }),
+                id: "mmoore",
+                pic: "assets/images/profile-placeholder.png",
+                name: 'Natasha Warshawsky',
+                role: 'Art Lead',
+                color: 'mwagar-color',
+                icon: 'create',
+                myrole: "woah boy",
+                aboutme: "Test123"
+            }),
+            Ember.Object.create({
+                id: "sdreisbach",
+                pic: "assets/images/profile-placeholder.png",
+                name: 'Ruben Guzman',
+                role: 'Programming Lead',
+                color: 'mwagar-color',
+                icon: 'code',
+                myrole: "woah boy",
+                aboutme: "Test123"
+            }),
+            Ember.Object.create({
+                id: "mwagar",
+                pic: "assets/images/mwagar_profile_pic.jpg",
+                name: 'Matthew Wagar',
+                role: 'Unity Developer',
+                color: 'mwagar-color',
+                icon: 'code',
+                myrole: "woah boy",
+                aboutme: "Test123"
+            }),
+            Ember.Object.create({
+                id: "jlipinski",
+                pic: "assets/images/profile-placeholder.png",
+                name: 'Tom Bavaria',
+                role: 'Artist',
+                color: 'mwagar-color',
+                icon: 'create',
+                myrole: "woah boy",
+                aboutme: "Test123"
+            }),
+            Ember.Object.create({
+                id: "cbelser",
+                pic: "assets/images/profile-placeholder.png",
+                name: 'Dimitri Couwenberg',
+                role: 'Artist',
+                color: 'mwagar-color',
+                icon: 'create',
+                myrole: "woah boy",
+                aboutme: "Test123"
+            }),
+            Ember.Object.create({
+                id: "clucas",
+                pic: "assets/images/profile-placeholder.png",
+                name: 'Steven Lilley',
+                role: 'Unity Developer',
+                color: 'mwagar-color',
+                icon: 'code',
+                myrole: "woah boy",
+                aboutme: "Test123"
+            }),
+
         ]
     }),
 
@@ -26,7 +77,7 @@ export default Ember.Controller.extend({
             profile.set('showDialog', false);
             this.set('selectedProfileTab', 'aboutme');
         },
-        openDialog(profile){
+        openDialog(profile) {
             profile.set('showDialog', true);
 
             //HACK fixes weird visual bug (sometimes)
